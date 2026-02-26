cdee972e4f502a29cea1902e806e80f2166b38e0
Joshkovu <joashkuteesa223@gmail.com>
2026-02-26 10:35:31 +0300

    refactor:improve security in the file

--- logify-backend/config/settings.py
+++ logify-backend/config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # type: ignore
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
