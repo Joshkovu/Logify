3fd9c04baf8bcff3b7dc62603cb8b73451f766cf
Joshkovu <joashkuteesa223@gmail.com>
2026-02-26 19:00:13 +0300

    refactor: organize the accounts app

--- logify-backend/apps/accounts/serializers.py
+++ logify-backend/apps/accounts/serializers.py
@@ -1,21 +1,6 @@
 from rest_framework import serializers
 
-from .models import (
-    EvaluationCriteria,
-    EvaluationRubrics,
-    Evaluations,
-    EvaluationScores,
-    FinalResults,
-    InternshipPlacements,
-    Organizations,
-    PlacementContacts,
-    PlacementStatusHistory,
-    StaffProfiles,
-    SupervisorReviews,
-    User,
-    WeeklyLogs,
-    WeeklyLogStatusHistory,
-)
+from .models import StaffProfiles, User
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -56,76 +41,4 @@ class UserSerializer(serializers.ModelSerializer):
 class StaffProfilesSerializer(serializers.ModelSerializer):
     class Meta:
         model = StaffProfiles
-        fields = "__all__"
-
-
-class OrganizationsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Organizations
-        fields = "__all__"
-
-
-class InternshipPlacementsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = InternshipPlacements
-        fields = "__all__"
-
-
-class PlacementStatusHistorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PlacementStatusHistory
-        fields = "__all__"
-
-
-class PlacementContactsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PlacementContacts
-        fields = "__all__"
-
-
-class WeeklyLogsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WeeklyLogs
-        fields = "__all__"
-
-
-class WeeklyLogStatusHistorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = WeeklyLogStatusHistory
-        fields = "__all__"
-
-
-class SupervisorReviewsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SupervisorReviews
-        fields = "__all__"
-
-
-class EvaluationRubricsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EvaluationRubrics
-        fields = "__all__"
-
-
-class EvaluationCriteriaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EvaluationCriteria
-        fields = "__all__"
-
-
-class EvaluationsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Evaluations
-        fields = "__all__"
-
-
-class EvaluationScoresSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EvaluationScores
-        fields = "__all__"
-
-
-class FinalResultsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FinalResults
         fields = "__all__"
