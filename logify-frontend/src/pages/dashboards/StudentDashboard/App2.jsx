aeed765731f577c63cef34e9c59097527c324210
Gatesk369 <kamugisha.emma369@gmail.com>
2026-03-04 21:45:55 +0300

    Added StudentDashboard, main dashbiard page, and the sidebar. Commented out some of Joash's lines.

---
+++ logify-frontend/src/pages/dashboards/StudentDashboard/App2.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import Sidebar from "./Sidebar.jsx";
+const App2 = () => {
+  return (
+    <Router>
+      <div className="flex min-h-screen">
+        <Sidebar />
+        <main className="flex-1 bg-gray-50 overflow-y-auto">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+};
+
+export default App2;
