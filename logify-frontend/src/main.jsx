43a8b3516843ab397067f6832165c2a308766bb3
kdujordan <kdu.jordan@gmail.com>
2026-03-09 00:02:49 +0300

    kaddu commited

--- logify-frontend/src/main.jsx
+++ logify-frontend/src/main.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import App3 from "./pages/dashboards/WorkplaceSupervisor/pages/App3";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <App3 />
+    <App />
   </React.StrictMode>,
 );
