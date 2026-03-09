4d713925a84ef9e437221f35a13c9d1d60d3d4bf
kdujordan <kdu.jordan@gmail.com>
2026-03-09 22:09:45 +0300

    kaddu commit4 - 09/03/2026

--- logify-frontend/src/pages/dashboards/WorkplaceSupervisor/components/dashboard/AssignedInternSection.jsx
+++ logify-frontend/src/pages/dashboards/WorkplaceSupervisor/components/dashboard/AssignedInternSection.jsx
@@ -20,11 +20,42 @@ const internsData = [
     course: "Information Systems",
     institution: "State University"
   },
+
+  // {
+  //   url: "https://api.dicebear.com/9.x/micah/svg?seed=Liam",
+  //   names: "Marcus Holloway",
+  //   course: "Computer Science",
+  //   institution: "Tech Institute of Technology"
+  // },
+  // {
+  //   url: "https://api.dicebear.com/9.x/micah/svg?seed=Zoe",
+  //   names: "Sarah Jenkins",
+  //   course: "Data Science",
+  //   institution: "Northfield Academy"
+  // },
+  // {
+  //   url: "https://api.dicebear.com/9.x/micah/svg?seed=Jasper",
+  //   names: "Arjun Mehta",
+  //   course: "Cybersecurity",
+  //   institution: "Global Open University"
+  // },
+  // {
+  //   url: "https://api.dicebear.com/9.x/micah/svg?seed=Heidi",
+  //   names: "Elena Rodriguez",
+  //   course: "Software Engineering",
+  //   institution: "Pacific Coast College"
+  // },
+  // {
+  //   url: "https://api.dicebear.com/9.x/micah/svg?seed=Felix",
+  //   names: "Jordan Smith",
+  //   course: "Business Analytics",
+  //   institution: "Metro State University"
+  // }
   
 ];
 const AssignedInternSection = () => {
   return (
-    <div  className="mt-3 rounded-lg p-6 border col-span-12 bg-white border-stone-300 shadow-inner">
+    <div  className="mt-3 rounded-lg p-6 border col-span-12 bg-white border-stone-300 shadow-inner h-auto">
       <h1 className="font-bold"> Assigned Interns</h1>
       <h2 className="text-gray-500 font-medium">OverView of interns under your supervision</h2>
       {internsData.map((intern, index) => (
--- logify-frontend/src/pages/dashboards/WorkplaceSupervisor/components/dashboard/Side_bar.jsx
+++ logify-frontend/src/pages/dashboards/WorkplaceSupervisor/components/dashboard/Side_bar.jsx
@@ -5,6 +5,7 @@ import { PiSignOut } from "react-icons/pi";
 import { ArrowLeftToLine,  ArrowRightToLine} from 'lucide-react';
 import { useLocation } from "react-router-dom";
 import Search from "./Search";
+import { ShieldCheck } from "lucide-react";
 
 
 
@@ -14,24 +15,36 @@ const Side_bar = ({children}) => {
   const [expanded, setExpanded] = React.useState(true);
   return (
     <aside className="h-screen flex">
-      <nav className="h-screen flex flex-col bg-white border-none shadow-sm rounded-r-lg">
-        <div className="p-4 pb-2 flex justify-between items-center">
-          <div className={`mr-1 overflow-hidden duration-300 transition-all
+      <nav className="h-full flex flex-col bg-white border-none shadow-sm">
+      <div className={`mb-12 px-4 ${expanded ? "" : "w-0 ml-0 hidden"} transition-all duration-200`}>
+        <div>
+          {" "}
+          <ShieldCheck className="text-white h-6 w-6" />
+        </div>
+        <div className="text-3xl text-black tracking-tighter text-gold flex items-center gap-2">
+          LOGIFY
+        </div>
+        <div className="text-[10px] uppercase tracking-[0.2em] font-bold text-gold/60 mt-1 ml-10">
+          Student Portal
+        </div>
+      </div>
+        <div className={`p-4 pb-4 flex justify-between items-center ${expanded ? "" : "pt-37"} `}>
+          <div className={`mr-1 overflow-hidden duration-200 transition-all
             ${expanded ? "w-52 ml-3" : "w-0 ml-0 hidden"}`}>
             <Search />
           </div>
-          <button onClick={() => setExpanded(curr => !curr )} className="p-1.5  rounded-full bg-gray-50 hover:bg-gray-100 ml-0.5 duration-300 transition-all shadow">
+          <button onClick={() => setExpanded(curr => !curr )} className="p-2  rounded-full bg-[#FCFBF2]  hover:bg-gray-100 ml-0.5 duration-200 transition-all shadow-lg">
             {expanded ? <ArrowLeftToLine size={20} className=" text-gray-700"/> : < ArrowRightToLine size={20} className=" text-gray-700"/>}
           </button>
         </div>
-        <p className={`flex text-sm justify-start ml-6 font-medium text-gray-500 py-2 overflow-hidden duration-300 transition-all
+        <p className={`flex text-sm justify-start ml-6 font-medium text-gray-500 py-2 overflow-hidden duration-200 transition-all
             ${expanded ? "w-52 ml-3" : "w-0 ml-0 hidden"}`}>
           MAIN NAVIGATION
         </p>
 
        <SidebarContext.Provider value={{expanded}}>
         
-        <ul className="flex-1 px-3">
+        <ul className={`flex-1 px-3 ${expanded ? "" : "mt-5"}`}>
           {children}
         </ul>
        </SidebarContext.Provider> 
@@ -42,7 +55,7 @@ const Side_bar = ({children}) => {
           <div
             className={`
             flex justify-between items-center
-            overflow-hidden duration-300 transition-all
+            overflow-hidden duration-200 transition-all
             ${expanded ? "w-52 ml-3" : "w-0 ml-0"}
             `}
           >
@@ -53,7 +66,7 @@ const Side_bar = ({children}) => {
           </div>
         </div>
 
-        <button className={`flex rounded-lg h-10 w-62 shadow my-6 mx-2 bg-gray-50 justify-center hover:bg-gray-200 pt-0.5 overflow-hidden duration-300 transition-all
+        <button className={`flex rounded-lg h-10 w-62 shadow my-6 mx-2 bg-gray-50 justify-center hover:bg-gray-200 pt-0.5 overflow-hidden duration-200 transition-all
             ${expanded ? "w-52 ml-3" : "w-0 ml-0 hidden"}`}>
           <div className={"flex justify-start p-1.5 "}>
             <PiSignOut className=" pt-0.5" />
@@ -81,18 +94,18 @@ export const SidebarItem = ({ icon, text, href }) => {
 
   return(
     <li  className={`
-      ${location.pathname === href ? "bg-maroonCustom text-white shadow-lg " : "hover:bg-red-50 text-gray-600"}
-    relative flex items-center 
-    font-medium rounded-md cursor-pointer  duration-300 transition-all
+      ${location.pathname === href ? "bg-maroonCustom text-white hover:shadow-sm " : "hover:bg-red-50 text-gray-600"}
+    relative flex items-center hover:-translate-y-0.5 hover:shadow-sm
+    font-medium rounded-md cursor-pointer  duration-200 transition-colors
      ${expanded ? "py-2 px-3 my-3" : "p-3 my-5"}
    
     `}>
-      <a href={href} className="flex">
+      <a href={href} className={`flex items-center w-full ${expanded ? "justify-start" : "justify-center"} transition-all duration-200  `}>
 
       {icon}
       <span className={`
             
-            duration-300 transition-all 
+            duration-200 transition-all 
             ${expanded ? "overflow-hidden w-52 ml-3" : "w-0 ml-0 hidden"}
             `} >{text}</span>
       </a>
