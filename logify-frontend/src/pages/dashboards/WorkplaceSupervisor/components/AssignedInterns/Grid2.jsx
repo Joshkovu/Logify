78de14b9e6a5dbc59920d8cf90314b4a49348065
kdujordan <kdu.jordan@gmail.com>
2026-03-10 03:09:16 +0300

    kaddu commit 1 - 10/03/2026

--- logify-frontend/src/pages/dashboards/WorkplaceSupervisor/components/AssignedInterns/Grid2.jsx
+++ logify-frontend/src/pages/dashboards/WorkplaceSupervisor/components/AssignedInterns/Grid2.jsx
@@ -1,8 +1,11 @@
 import AssignedInternAnalytics from "../dashboard/InternsSection/AssignedInternAnalytics";
+import Intern_details from "./Intern_details";
+
 
 const Grid2 = () => {
   return <div className="px-4 grid gap-3 grid-cols-12 py-8">
     <AssignedInternAnalytics />
+    <Intern_details />
   </div>;
 };
 
