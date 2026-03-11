9f609f143500f25719e7187bf28de3978348914c
Daizy499 <dchemutai499@gmail.com>
2026-03-11 16:23:56 +0300

    the unauthorized access page

---
+++ logify-frontend/src/components/UnauthorizedAccess.jsx
@@ -0,0 +1,13 @@
+const UnauthorizedAccess= ()=>{
+    return(
+        <div>
+            
+            <h1>403</h1>
+            <h3>Forbidden</h3>
+            <p> Access to the requested resource is forbidden.</p>
+            <button onclick="window.location.href='/home'">Go to Home</button>
+
+        </div>
+    )
+}
+export default UauthorizedAccess;
