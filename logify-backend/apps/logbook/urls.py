ae91490458ecc91c0f8aca0bba3a804fb529e416
Joshkovu <joashkuteesa223@gmail.com>
2026-03-12 18:27:37 +0300

    feat:add test that ensures that the workplace_ supervisors are  the only ones who can approve and reject a weekly log

--- logify-backend/apps/logbook/urls.py
+++ logify-backend/apps/logbook/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("create_weekly_log/", views.CreateWeeklyLogAPIView.as_view(), name="create_weekly_log"),  # type: ignore
     path("update_weekly_log/<str:log_id>/", views.UpdateWeeklyLogAPIView.as_view(), name="update_weekly_log"),  # type: ignore
-    path("submit_weekly_log/<str:log_id>/", views.SubmitWeeklyLogAPIView.as_view(), name="submit_weekly_log"),  # type: ignore
-    path("approve_weekly_log/<str:log_id>/", views.ApproveWeeklyLogAPIView.as_view(), name="approve_weekly_log"),  # type: ignore
-    path("reject_weekly_log/<str:log_id>/", views.RejectWeeklyLogAPIView.as_view(), name="reject_weekly_log"),  # type: ignore
+    path("submit_weekly_log/<str:log_id>/", views.SubmitWeeklyLogsAPIView.as_view(), name="submit_weekly_log"),  # type: ignore
+    path("approve_weekly_log/<str:log_id>/", views.ApproveWeeklyLogsAPIView.as_view(), name="approve_weekly_log"),  # type: ignore
+    path("reject_weekly_log/<str:log_id>/", views.RejectWeeklyLogsAPIView.as_view(), name="reject_weekly_log"),  # type: ignore
 ]
