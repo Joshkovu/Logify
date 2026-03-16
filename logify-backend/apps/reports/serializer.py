c9797b53c3ea39ce33de3bc89f7c9f9742bfc331
Joshkovu <joashkuteesa223@gmail.com>
2026-03-16 18:32:34 +0300

    refactor:ensure that there is a friendly error incase the report has no weekly logs

--- logify-backend/apps/reports/serializer.py
+++ logify-backend/apps/reports/serializer.py
@@ -3,7 +3,14 @@ from rest_framework import serializers
 from .models import InternshipReport
 
 
-class InternshipReportSerializer(serializers.Serializer):
+class InternshipReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = InternshipReport
         fields = "__all__"
+
+    def validate(self, attrs):
+        if attrs["internship_end"] <= attrs["internship_start"]:
+            raise serializers.ValidationError(
+                "Internship end date must be after internship start date"
+            )
+        return attrs
--- logify-backend/apps/reports/views.py
+++ logify-backend/apps/reports/views.py
@@ -1,4 +1,5 @@
 from apps.logbook.models import WeeklyLogs
+from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView, Response
 
 from .models import InternshipReport
@@ -9,11 +10,12 @@ from .models import InternshipReport
 class WeeklyLogsReportAPIView(APIView):
     def generate_internship_report(self, student):
         logs = WeeklyLogs.objects.filter(placement__intern_id=student).order_by("week_number")
-
+        if not logs.exists():
+            raise NotFound("No logs found for the student")
         report = InternshipReport.objects.create(
             student_id=student,
-            internship_start=logs.first().week_start_date if logs.exists() else None,  # type: ignore
-            internship_end=logs.last().week_end_date if logs.exists() else None,  # type: ignore
+            internship_start=logs.first().week_start_date,  # type: ignore
+            internship_end=logs.last().week_end_date,  # type: ignore
             logs="\n\n".join(
                 [
                     f"Week {log.week_number}:\nActivities: {log.activities}\nChallenges: {log.challenges}\nLearnings: {log.learnings}"
@@ -24,8 +26,11 @@ class WeeklyLogsReportAPIView(APIView):
         return report
 
     def get(self, request, student_id):
-        report = self.generate_internship_report(student_id)
-
+        report = (
+            InternshipReport.objects.filter(student_id=student_id).order_by("-created_at").first()
+        )
+        if not report:
+            report = self.generate_internship_report(student_id)
         return Response(
             {
                 "report_id": report.id,  # type: ignore
