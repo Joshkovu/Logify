b6e36c1cacb3e6f44e82fb525ca8af3cb98110ac
Joshkovu <joashkuteesa223@gmail.com>
2026-03-16 18:06:53 +0300

    refactor:adjust these files to support the tests to pass

--- logify-backend/apps/reports/urls.py
+++ logify-backend/apps/reports/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("weekly_logs_report/", views.WeeklyLogsReportAPIView.as_view(), name="weekly_logs_report"),  # type: ignore
+    path("weekly_logs_report/<int:student_id>/", views.WeeklyLogsReportAPIView.as_view(), name="weekly_logs_report"),  # type: ignore
 ]
