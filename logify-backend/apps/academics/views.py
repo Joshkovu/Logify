1c3f10a8246fa523e81a2d58b21935bdaea08ba6
Gatesk369 <kamugisha.emma369@gmail.com>
2026-03-20 19:17:44 +0300

    Rewrote the logic to limit academics objects viewing access to specific users.

--- logify-backend/apps/academics/views.py
+++ logify-backend/apps/academics/views.py
@@ -1,5 +1,8 @@
 # from django.shortcuts import render
-from rest_framework.exceptions import NotFound
+from apps.accounts.models import User
+from apps.placements.models import InternshipPlacements
+from rest_framework.exceptions import NotFound, PermissionDenied
+from rest_framework.permissions import BasePermission, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -12,7 +15,82 @@ from .serializer import (
 
 
 # Create your views here.
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            return False
+        return request.user.is_superuser or request.user.role == User.INTERNSHIP_ADMIN
+
+
+def get_accessible_institution_ids(user):
+    if user.is_superuser or user.role == User.INTERNSHIP_ADMIN:
+        return Institutions.objects.values_list("id", flat=True)
+
+    if user.role == User.STUDENT:
+        return InternshipPlacements.objects.filter(intern=user).values_list(
+            "institution_id", flat=True
+        )
+
+    if user.role == User.WORKPLACE_SUPERVISOR:
+        return InternshipPlacements.objects.filter(workplace_supervisor=user).values_list(
+            "institution_id", flat=True
+        )
+
+    if user.role == User.ACADEMIC_SUPERVISOR:
+        return InternshipPlacements.objects.filter(academic_supervisor=user).values_list(
+            "institution_id", flat=True
+        )
+
+    return Institutions.objects.none().values_list("id", flat=True)
+
+
+def get_accessible_department_ids(user):
+    if user.is_superuser or user.role == User.INTERNSHIP_ADMIN:
+        return Departments.objects.values_list("id", flat=True)
+
+    if user.role == User.STUDENT:
+        return Departments.objects.filter(
+            institution_id__in=InternshipPlacements.objects.filter(intern=user).values_list(
+                "institution_id", flat=True
+            )
+        ).values_list("id", flat=True)
+
+    if user.role == User.WORKPLACE_SUPERVISOR:
+        return Departments.objects.filter(
+            institution_id__in=InternshipPlacements.objects.filter(
+                workplace_supervisor=user
+            ).values_list("institution_id", flat=True)
+        ).values_list("id", flat=True)
+
+    if user.role == User.ACADEMIC_SUPERVISOR:
+        return Departments.objects.filter(
+            institution_id__in=InternshipPlacements.objects.filter(
+                academic_supervisor=user
+            ).values_list("institution_id", flat=True)
+        ).values_list("id", flat=True)
+
+    return Departments.objects.none().values_list("id", flat=True)
+
+
+def get_accessible_programme_ids(user):
+    if user.is_superuser or user.role == User.INTERNSHIP_ADMIN:
+        return Programmes.objects.values_list("id", flat=True)
+
+    if user.role == User.STUDENT:
+        return InternshipPlacements.objects.filter(intern=user).values_list(
+            "programme_id", flat=True
+        )
+
+    if user.role == User.ACADEMIC_SUPERVISOR:
+        return InternshipPlacements.objects.filter(academic_supervisor=user).values_list(
+            "programme_id", flat=True
+        )
+
+    return Programmes.objects.none().values_list("id", flat=True)
+
+
 class InstitutionsListView(APIView):
+    permission_classes = [IsAdmin]
 
     def get(self, request):
         institutions = Institutions.objects.all()
@@ -21,9 +99,12 @@ class InstitutionsListView(APIView):
 
 
 class InstitutionsDetailView(APIView):
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, pk):
         institution = self.get_object(pk)
+        if pk not in get_accessible_institution_ids(request.user):
+            raise PermissionDenied("You do not have access to this institution")
         serializer = InstitutionsSerializer(institution)
         return Response(serializer.data)
 
@@ -35,6 +116,7 @@ class InstitutionsDetailView(APIView):
 
 
 class DepartmentsListView(APIView):
+    permission_classes = [IsAdmin]
 
     def get(self, request):
         departments = Departments.objects.all()
@@ -43,9 +125,12 @@ class DepartmentsListView(APIView):
 
 
 class DepartmentsDetailView(APIView):
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, pk):
         department = self.get_object(pk)
+        if pk not in get_accessible_department_ids(request.user):
+            raise PermissionDenied("You do not have access to this department.")
         serializer = DepartmentsSerializer(department)
         return Response(serializer.data)
 
@@ -57,13 +142,19 @@ class DepartmentsDetailView(APIView):
 
 
 class InstitutionDepartmentsListView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request, institution_id):
+        accessible_institution_ids = get_accessible_institution_ids(request.user)
+        if institution_id not in accessible_institution_ids:
+            raise PermissionDenied("You do not have access to this institution's departments")
         departments = Departments.objects.filter(institution_id=institution_id)
         serializer = DepartmentsSerializer(departments, many=True)
         return Response(serializer.data)
 
 
 class ProgrammesListView(APIView):
+    permission_classes = [IsAdmin]
 
     def get(self, request):
         programmes = Programmes.objects.all()
@@ -72,9 +163,12 @@ class ProgrammesListView(APIView):
 
 
 class ProgrammesDetailView(APIView):
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, pk):
         programme = self.get_object(pk)
+        if pk not in get_accessible_programme_ids(request.user):
+            raise PermissionDenied("You do not have access to this programme.")
         serializer = ProgrammesSerializer(programme)
         return Response(serializer.data)
 
@@ -86,7 +180,12 @@ class ProgrammesDetailView(APIView):
 
 
 class DepartmentProgrammesListView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request, department_id):
+        accessible_department_ids = get_accessible_department_ids(request.user)
+        if department_id not in accessible_department_ids:
+            raise PermissionDenied("You do not have accedd to this department's programmes")
         programmes = Programmes.objects.filter(department_id=department_id)
         serializer = ProgrammesSerializer(programmes, many=True)
         return Response(serializer.data)
