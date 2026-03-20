c3ff3c9ac6b4b7bbef8f3195bfda2195d63491af
Joshkovu <joashkuteesa223@gmail.com>
2026-03-20 13:25:02 +0300

    fix:make tests pass by adding organizations to urls.py in the config file

--- logify-backend/apps/organizations/urls.py
+++ logify-backend/apps/organizations/urls.py
@@ -4,9 +4,7 @@ from .views import OrganizationDetailView, OrganizationListCreateView
 
 urlpatterns = [
     path(
-        "/createOrganization", OrganizationListCreateView.as_view(), name="organization-list-create"
+        "createOrganization", OrganizationListCreateView.as_view(), name="organization-list-create"
     ),
-    path(
-        "/getOrganization/<int:pk>/", OrganizationDetailView.as_view(), name="organization-detail"
-    ),
+    path("getOrganization/<int:pk>/", OrganizationDetailView.as_view(), name="organization-detail"),
 ]
