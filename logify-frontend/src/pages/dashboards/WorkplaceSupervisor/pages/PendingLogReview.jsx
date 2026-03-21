9f810b132adb8d35909034273b5d078ee691bcba
susanaudrey85-eng <susanaudrey85@gmail.com>
2026-03-21 17:12:00 +0300

    Dashboard darkmode

--- logify-frontend/src/pages/dashboards/WorkplaceSupervisor/pages/PendingLogReview.jsx
+++ logify-frontend/src/pages/dashboards/WorkplaceSupervisor/pages/PendingLogReview.jsx
@@ -1,11 +1,13 @@
-import Grid3 from "../components/LogsReview/Grid3"
+import Grid3 from "../components/LogsReview/Grid3";
 
 const PendingLogReview = () => {
   return (
-    <div className='bg-[#FCFBF8]  pb-4 shadow h-screen w-full p-16 transition-all overflow-y-auto'>
-      <h1 className=' font-bold text-2xl '>Pending Log Reviews</h1>
-      <h2 className='font-light text-lg text-gray-600'>Welcome back, John! Here your supervision overview</h2>
-      
+    <div className="bg-[#FCFBF8]  pb-4 shadow h-screen w-full p-16 transition-all overflow-y-auto">
+      <h1 className=" font-bold text-2xl ">Pending Log Reviews</h1>
+      <h2 className="font-light text-lg text-gray-600">
+        Welcome back, John! Here your supervision overview
+      </h2>
+
       <Grid3 />
     </div>
   );
